250af060cc1e2dd85c73d52ead67dcac9122a783
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2022-12-01 22:45:14 +0200

    day 1

---
+++ day1/p1/solve.py
@@ -0,0 +1,47 @@
+import os.path
+
+INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def solve(s: list[str]) -> int:
+    res = [0]
+    for line in s:
+        stripped = line.strip()
+        if stripped:
+            res[-1] += int(stripped)
+        else:
+            res.append(0)
+    return max(res)
+
+
+TEST_INPUT = """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+TEST_EXPECTED = 24000
+
+
+def test() -> None:
+    assert solve(TEST_INPUT.split('\n')) == TEST_EXPECTED
+
+
+def main() -> int:
+    with open(INPUT) as file:
+        print(solve(file.readlines()))
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
